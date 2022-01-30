eafe38702d317e164a7aa3c6c851817f4309945f
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2022-01-30 18:21:41 +0000

    add assessment methods

--- Java_version/src/main/java/ComparisonStep.java
+++ Java_version/src/main/java/ComparisonStep.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,8 +38,8 @@ public class ComparisonStep {
         return error / (double) vertices.size();
     }
 
-    public double getHausorffDistance(List<Vertex> vertices1, List<Vertex> vertices2) {
-        double maxDA = 0d;
+    public double getMinDistance(List<Vertex> vertices1, List<Vertex> vertices2) {
+        double maxDistance = 0d;
         for (Vertex vertex1 : vertices1) {
             double minDist = Double.POSITIVE_INFINITY;
             for (Vertex vertex2 : vertices2) {
@@ -44,37 +48,46 @@ public class ComparisonStep {
                     minDist = distance;
                 }
             }
-            if (minDist > maxDA) {
-                maxDA = minDist;
+            if (minDist > maxDistance) {
+                maxDistance = minDist;
             }
         }
+        return maxDistance;
+    }
 
-        double maxDB = 0d;
-        for (Vertex vertex1 : vertices2) {
+    public List<Double> getMinDistanceDistribution(List<Vertex> vertices1, List<Vertex> vertices2) {
+        List<Double> vertexDistances = new ArrayList<>(vertices1.size());
+        for (Vertex vertex1 : vertices1) {
             double minDist = Double.POSITIVE_INFINITY;
-            for (Vertex vertex2 : vertices1) {
+            for (Vertex vertex2 : vertices2) {
                 double distance = MathUtils.minusVector(vertex1.getCoords(), vertex2.getCoords()).getMod();
                 if (distance <= minDist) {
                     minDist = distance;
                 }
             }
-            if (minDist > maxDB) {
-                maxDB = minDist;
-            }
+            vertexDistances.add(minDist);
         }
+        return vertexDistances;
+    }
+
+    public double getHausorffDistance(List<Vertex> vertices1, List<Vertex> vertices2) {
+        double maxDA = getMinDistance(vertices1, vertices2);
+        double maxDB = getMinDistance(vertices2, vertices1);
 
         return Math.max(maxDA, maxDB);
     }
 
-    public void getHausorffDistribution(){
+    public void writeDistribution(List<Vertex> vertices1, List<Vertex> vertices2) throws IOException {
+        List<Double> distribution1 = getMinDistanceDistribution(vertices1, vertices2);
+        String fileName = "C:\\Users\\tangj\\Downloads\\distribution.csv";
+        BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+        for (Double distance : distribution1) {
+            bw.write(Double.toString(distance) + "\n");
+        }
 
+        bw.close();
     }
 
-    public void writeDistribution(){
-
-    }
-
-    //static method to get the normal vector for vertices
     private static Vector3d getNormalForVertex(Vertex vertex, List<Triangle> triangles) {
         List<Integer> neighbours = vertex.getTriangleIndices();
         Vector3d vNorm = new Vector3d(0, 0, 0);
@@ -86,6 +99,7 @@ public class ComparisonStep {
         return MathUtils.dotVal(1d / (double) neighbours.size(), vNorm);
     }
 
+    //static method to get the normal vector for vertices
     public static Map<Integer, Vector3d> getNormalForVertices(InputModel inputModel) {
         List<Vertex> vertices = inputModel.getVertices();
         List<Triangle> triangles = inputModel.getTriangles();
--- Java_version/src/main/java/Square3Scheme.java
+++ Java_version/src/main/java/Square3Scheme.java
@@ -23,25 +23,15 @@ public class Square3Scheme {
         this.edges = edges;
     }
 
-    public Vector3d insertPointRegular1(Triangle triangleEach) {
-        Vector3d coord = new Vector3d(0, 0, 0);
-        for (Vertex v : triangleEach.vertices) {
-            coord = MathUtils.addVector(coord, v.getCoords());
-        }
-        return MathUtils.dotVal(1d / 3d, coord);
-    }
-
-    public void insertPointIrregular(Triangle triangleEach) {
-        for (Vertex v : triangleEach.getVertices()) {
-            if (!v.isRegular()) {
-
-            }
-        }
-    }
-
-    public void getPtsInOrder(Triangle triangleEach, Vertex v) {
+    public List<Vertex> getPtsInOrder(Triangle triangleEach, Vertex v) {
         List<Edge> edgesConnect = triangleEach.getConnectedEdges(v);
+        List<Integer> trianglesIndexNear = v.getTriangleIndices();
+        List<Triangle> trianglesNear = new ArrayList<>();
+        for (Integer triIndex : trianglesIndexNear) {
+            trianglesNear.add(this.triangles.get(triIndex));
+        }
         List<Vertex> verticesNear = new ArrayList<>();
+
         for (Edge edge : edgesConnect) {
             if (edge.getA().equals(v)) {
                 verticesNear.add(edge.getB());
@@ -50,16 +40,90 @@ public class Square3Scheme {
             }
         }
 
-        //todo: same as the one in modified butterfly
+        Vertex vOld = verticesNear.get(1);
+        int iterN = 0;
+        int maxN = trianglesNear.size() * v.getNumVertices() * 2;
 
+        while (verticesNear.size() != v.getNumVertices()) {
+            if (iterN > maxN) {
+                break;
+            }
+            for (Triangle triangle : trianglesNear) {
+
+                if (triangle.containVertices(vOld, v)) {
+                    Vertex vRemain = triangle.getRemain(v, vOld);
+                    if (!verticesNear.contains(vRemain)) {
+                        verticesNear.add(vRemain);
+                        vOld = vRemain;
+                    }
+                }
+                iterN += 1;
+            }
+            iterN += 1;
+        }
+        return verticesNear;
     }
 
-    public Map<Integer, Vector3d> insertPointRegular1() {
+    private double getCoeff(int n, int j) {
+        return (1d / 9d + 2d / 3d * Math.cos(2 * Math.PI * j / n) + 2d / 9d * Math.cos(4 * Math.PI * j / n)) / n;
+    }
+
+    public Vector3d insertPointIrregular(Triangle triangleEach) {
+        //Map<Integer, Vector3d> stencils = new HashMap<>();
+        List<Vector3d> coords = new ArrayList<>();
+        for (Vertex v : triangleEach.getVertices()) {
+            if (!v.isRegular()) {
+                int n = v.getNumVertices();
+                List<Vertex> verticesNear = getPtsInOrder(triangleEach, v);
+                Vector3d sum = new Vector3d(0, 0, 0);
+                switch (n) {
+                    case 3:
+                        double[] coeffs = new double[]{7d / 27d, -2d / 27d, -2d / 27d};
+                        for (int i = 0; i < verticesNear.size(); i++) {
+                            Vertex vEach = verticesNear.get(i);
+                            MathUtils.addVector(sum, MathUtils.dotVal(coeffs[i], vEach.getCoords()));
+                        }
+                    case 4:
+                        coeffs = new double[]{7d / 36d, 1d / 27d, 1d / 27d, -5d / 36d};
+                        for (int i = 0; i < verticesNear.size(); i++) {
+                            Vertex vEach = verticesNear.get(i);
+                            MathUtils.addVector(sum, MathUtils.dotVal(coeffs[i], vEach.getCoords()));
+                        }
+                    default:
+                        for (int i = 0; i < verticesNear.size(); i++) {
+                            Vertex vEach = verticesNear.get(i);
+                            double coeff = getCoeff(n, i);
+                            MathUtils.addVector(sum, MathUtils.dotVal(coeff, vEach.getCoords()));
+                        }
+                }
+                //stencils.put(v.getIndex(), sum);
+                coords.add(sum);
+            }
+        }
+        int nExtra = coords.size();
+        Vector3d coordNewVertex = new Vector3d(0, 0, 0);
+        for (Vector3d coordEach : coords) {
+            MathUtils.addVector(coordNewVertex, coordEach);
+        }
+        return MathUtils.dotVal(1d / (double) nExtra, coordNewVertex);
+    }
+
+    public Vector3d insertPointRegular1(Triangle triangleEach) {
+        Vector3d coord = new Vector3d(0, 0, 0);
+        for (Vertex v : triangleEach.vertices) {
+            coord = MathUtils.addVector(coord, v.getCoords());
+        }
+        return MathUtils.dotVal(1d / 3d, coord);
+    }
+
+    public Map<Integer, Vector3d> insertPoints() {
         int index = vertices.size();
         Map<Integer, Vector3d> vertexMap = new HashMap<Integer, Vector3d>();
         for (Triangle triangleEach : this.triangles) {
             if (triangleEach.isNearExtraordinary()) {
-
+                Vector3d coord = this.insertPointIrregular(triangleEach);
+                vertexMap.put(index, coord);
+                index += 1;
             } else {
                 vertexMap.put(index, this.insertPointRegular1(triangleEach));
                 index += 1;
